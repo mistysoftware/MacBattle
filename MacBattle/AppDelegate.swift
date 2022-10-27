d49af6fbec4a8a0344afa09bd58839719e3e2f9e
keithmalkin <keithmalkin@fastmail.net>
2022-10-27 08:34:20 -0400

    Xcode 14.1 template

---
+++ MacBattle/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MacBattle
+//
+//  Created by Keith Malkin on 10/27/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+		return true
+	}
+
+
+}
+
